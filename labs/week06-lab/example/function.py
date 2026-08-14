905575cbc2559d6cdb97756d0c863cbb61edf6dc
Jirapha Sodsee <jirapha.so@ku.th>
2026-08-14 12:10:30 +0700

    function

--- labs/week06-lab/example/function.py
+++ labs/week06-lab/example/function.py
@@ -98,7 +98,7 @@ def get_circle_info(radius):
 print("Circle calculations:")
 radius = 5
 area, circumference = get_circle_info(radius)
-print(f"Circle with radius {radius}:")
+  print(f"Circle with radius {radius}:")
 print(f"Area: {area:.2f}")
 print(f"Circumference: {circumference:.2f}")
 print()
