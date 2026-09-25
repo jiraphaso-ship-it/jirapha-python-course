738d6b9f2f646643c9eaaf29739621512797b999
Jirapha Sodsee <jirapha.so@ku.th>
2026-09-25 12:00:07 +0700

    quiweek7

--- assignments/week07/quiz-1.py
+++ assignments/week07/quiz-1.py
@@ -12,13 +12,37 @@ class Rectangle:
 
     # Method to get the area
     def get_area(self):
-        pass
+        return self.length * self.width
 
     # Method to get the perimeter
     def get_perimeter(self):
-        pass
+        return 2 * (self.length + self.width)
 
 
 rect = Rectangle(10, 5)
 print(rect.get_area())       # Should print 50
 print(rect.get_perimeter())  # Should print 30
+
+
+
+""" 
+ขอให้เขียนคลาส circle ที่ทำงานคล้ายคลึงกับคลาส rectangle พร้อมตัวอย่าง
+"""
+class Circle:
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    # Method to get the area
+    def get_area(self):
+        return 3.1416 * self.radius * self.radius
+
+    # Method to get the perimeter
+    def get_perimeter(self):
+        return 2 * 3.1416 * self.radius
+
+
+circle = Circle(10)
+print(circle.get_area())       # Should print 314.16
+print(circle.get_perimeter())  # Should print 62.832
+ 
